230ccc59b114a0684abade9e704ee31f112e90b8
fesloth <sazzaliee@gmail.com>
2024-11-17 19:20:27 +0800

    work done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
+import Work from "./components/Work";
 
 const App = () => {
 
@@ -14,6 +15,7 @@ const App = () => {
       <Hero />
       <About />
       <Skill />
+      <Work />
     </main>
     </>
   )
