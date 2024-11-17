8961cc4691bb5ff54d96a7d056627ee9fdde86fc
fesloth <sazzaliee@gmail.com>
2024-11-17 21:50:29 +0800

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// node modules
+
+import { ReactLenis } from 'lenis/react'
 // components
 
 import Header from "./components/Header";
@@ -7,11 +10,12 @@ import Skill from "./components/Skill";
 import Work from "./components/Work";
 import Certif from "./components/Certif";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
 
   return ( 
-  <>
+  <ReactLenis root>
   <Header />
     <main>
       <Hero />
@@ -21,7 +25,8 @@ const App = () => {
       <Certif />
       <Contact />
     </main>
-    </>
+    <Footer />
+    </ReactLenis>
   )
 
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx';
 // CSS link
 import './index.css';
+import 'lenis/dist/lenis.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
